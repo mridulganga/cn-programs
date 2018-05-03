a851cd135b27a99b10120287b4c6decc92556988
mridulganga <mridul.kepler@gmail.com>
2018-05-04 00:44:58 +0530

    p7 fix

--- p7.c
+++ p7.c
@@ -2,26 +2,19 @@
 #include <string.h>
 #include <unistd.h>
 
-int display(char out[]){
-  write(1,out,strlen(out));
-  write(1,"\n",1);
-}
-
 int crc(char input[],char output[],char gp[],int mode){
   int i,j;
   strcpy(output,input);
 
   if (mode==0){
-    for(i=1;i<strlen(gp);i++){
+    for(i=1;i<strlen(gp);i++)
       strcat(output,"0");
-    }
   }
-  display(output);
 
   for (i=0;i<strlen(input);i++){
     if (output[i]=='1'){
       for (j=0;j<strlen(gp);j++){
-        if ((output[i+j]=='0' && gp[j]=='0') || (output[i+j]=='1' && gp[j]=='1'))
+        if ((output[i+j]=='0') && (gp[j]=='0') || (output[i+j]=='1') && (gp[j]=='1'))
           output[i+j]='0';
         else
           output[i+j]='1';
@@ -32,11 +25,10 @@ int crc(char input[],char output[],char gp[],int mode){
     if (output[i]=='1') return 1;
   }
   return 0;
-
 }
 
 int main(){
-  char input[50],output[50],r[50],gp[18]="100010000000100001";
+  char input[50],output[50],r[50],gp[18]="10001000000100001";
 
   printf("Enter code: ");
   scanf("%s",input);
