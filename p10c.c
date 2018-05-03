a1e87c33c5db2af18238560e309926a1978f5dfc
mridulganga <mridul.kepler@gmail.com>
2018-05-04 00:27:34 +0530

    p10 tcp->udp

--- p10c.c
+++ p10c.c
@@ -8,18 +8,21 @@ int main(){
   int sfd,n;
   char hostname[100],contents[1024]={0};
   struct sockaddr_in server_addr;
+  socklen_t addrsize;
 
-  sfd = socket(AF_INET,SOCK_STREAM,0);
+  sfd = socket(AF_INET,SOCK_DGRAM,0);
   server_addr.sin_port = htons(8090);
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  connect(sfd,(struct sockaddr*) &server_addr, sizeof server_addr);
+  // connect(sfd,(struct sockaddr*) &server_addr, sizeof server_addr);
   printf("Enter host: ");
   scanf("%s",hostname);
-  send(sfd,hostname,strlen(hostname),0);
 
-  n=recv(sfd,contents,1024,0);
+  addrsize = (sizeof server_addr);
+  sendto(sfd,hostname,strlen(hostname),0,(struct sockaddr*)&server_addr,(socklen_t)addrsize);
+
+  n=recvfrom(sfd,contents,1024,0,(struct sockaddr*)&server_addr,(socklen_t*)&addrsize);
   write(1,contents,n);
 
   close(sfd);
--- p10s.c
+++ p10s.c
@@ -12,11 +12,10 @@ int main(){
   int sfd,cfd,fd,n;
   char hostname[100]={0},contents[1024]={0};
   struct sockaddr_in address;
-  // struct sockaddr_storage sstore;
-  // socklen_t addrsize;
   struct hostent *lh;
+  socklen_t addrsize;
 
-  sfd = socket(AF_INET,SOCK_STREAM,0);
+  sfd = socket(AF_INET,SOCK_DGRAM,0);
   address.sin_family = AF_INET;
   address.sin_port = htons(8090);
   address.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -25,15 +24,17 @@ int main(){
   listen(sfd,5);
 
   // addrsize = sizeof sstore;
-  while(cfd = accept(sfd,(struct sockaddr*)NULL,NULL)){
+  addrsize = (sizeof address);
+  while(recvfrom(sfd,hostname,100,0,(struct sockaddr*)&address,(socklen_t*)&addrsize)){
     // cfd = accept(sfd,(struct sockaddr*) &sstore,&addrsize);
-    memset(hostname,0,strlen(hostname));
-    recv(cfd,hostname,100,0);
     printf("Hostname : %s\n",hostname);
 
     lh = gethostbyname(hostname);
     // address.sin_addr.S_addr=(u_long)lh->h_addr;
-    send(cfd,inet_ntoa(*(struct in_addr *)lh->h_addr_list[0]),strlen(inet_ntoa(*(struct in_addr *)lh->h_addr_list[0])),0);
+    sendto(sfd,inet_ntoa(*(struct in_addr *)lh->h_addr_list[0]),
+                strlen(inet_ntoa(*(struct in_addr *)lh->h_addr_list[0])),
+                0,(struct sockaddr*)&address,(socklen_t)addrsize);
+    memset(hostname,0,strlen(hostname));
 
   }
   close(sfd);
